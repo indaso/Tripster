26654a0379a01f6ca0176fba4b2bdf40c075fa89
indaso <indaso5@gmail.com>
2014-12-10 17:33:53 -0500

    added User who could sign in

--- user/login.js
+++ user/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var oracle = require('oracle');
+global.User = require('./user');
+global.currUser = new User();
 
 /* When user tries to log on, verify his information.
 If it is correct, redirect him/her to his/her profile
@@ -14,41 +16,52 @@ var connectData = {
   password: "CS450&frdS"
 };
 
-router.get('/login', function(req, res) {
-  res.render('login', { title: 'Tripster:Login' });
+router.get('/login', function (req, res) {
+  res.render('login', {
+    title: 'Tripster:Login'
+  });
 });
 
 
-router.post('/login', function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
+router.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
 
-   	//Print for testing/building
-   	console.log("LOGIN");
-   	console.log(username);
-   	console.log(password);
+  //Print for testing/building
+  console.log("LOGIN");
+  console.log(username);
+  console.log(password);
 
-    //Connect to database
-    oracle.connect(connectData, function(err, connection) {
-        if (err) {console.log("Error connecting to db:", err); return; }
+  //Connect to database
+  oracle.connect(connectData, function (err, connection) {
+    if (err) {
+      console.log("Error connecting to db:", err);
+      return;
+    }
 
-        //Query database for username's password
-        //userid for testing
-        var query = 'SELECT PASSWORD, USER_ID FROM USERS WHERE PASSWORD = ' + "'" + password + "'" + "AND USER_ID = '" + username + "'";
-        console.log('QUERY = ' + query); 
-        connection.execute(query, [], function(err, results) {
-            if (err) {console.log("Error executing query:", err); return; }
+    //Query database for username's password
+    //userid for testing
+    var query = 'SELECT PASSWORD, USER_ID FROM USERS WHERE PASSWORD = ' + "'" + password + "'" +
+      "AND USER_ID = '" + username + "'";
+    console.log('QUERY = ' + query);
+    connection.execute(query, [], function (err, results) {
+      if (err) {
+        console.log("Error executing query:", err);
+        return;
+      }
 
-            console.log(results);   //print for testing
-            if(results.length == 1) {
-              console.log("SUCCESSFUL LOGIN"); 
-              res.redirect('/myprofile');
-               }
-              else console.log("WRONG")
+      console.log(results); //print for testing
+      if (results.length == 1) {
+        currUser = new User(username, password);
+        currUser.signed_up = true;
+        console.log("SUCCESSFUL LOGIN");
+        console.log("User name=" + currUser.username + " User password=" + currUser.password);
+        res.redirect('/myprofile');
+      } else console.log("WRONG");
 
-            connection.close();     //close db connection after query
-        });
+      connection.close(); //close db connection after query
     });
+  });
 });
 
 
--- user/user.js
+++ user/user.js
@@ -1,55 +1,7 @@
-var oracle = require('oracle');
-
-var connectData = {
-	hostname: "tripsterdb.cmjcmauyxdtp.us-east-1.rds.amazonaws.com",
-	port: 1521,
-	database: "Wally",
-	user: "masterusr",
-	password: "CS450&frdS"
-};
-
 var User = function (username, password) {
 	this.username = username;
 	this.password = password;
-
-
-
-	function findOne(username, password, callback) {
-		//Connect to database
-		oracle.connect(connectData, function (err, connection) {
-			if (err) {
-				console.log("Error connecting to db:", err);
-				return;
-			}
-
-			//Query database for username's password
-			//userid for testing
-			var query = 'SELECT PASSWORD, USER_ID FROM USERS WHERE PASSWORD = ' + "'" + password + "'" +
-				"AND USER_ID = '" + username + "'";
-			console.log('QUERY = ' + query);
-			connection.execute(query, [], function (err, results) {
-				if (err) {
-					console.log("Error executing query:", err);
-					return;
-				}
-
-				user = results;
-				console.log(results); //print for testing
-				if (results.length == 1) {
-					console.log("SUCCESSFUL LOGIN");
-					res.redirect('/myprofile');
-				} else console.log("WRONG");
-
-				connection.close(); //close db connection after query
-			});
-		});
-
-	}
-
-	return {
-		findOne: findOne,
-		findById: findById
-	};
+	this.signed_up = false;
 };
 
 module.exports = User;
