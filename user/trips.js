5d75e3718a9caea0b4ce4f6a55a6871319e84cbc
Jenny Hu <jenhu@sas.upenn.edu>
2014-12-14 05:37:07 -0500

    trip updated

--- user/trips.js
+++ user/trips.js
@@ -1,16 +1,10 @@
-/*jslint node: true */
-"use strict";
-
 var express = require('express');
 var router = express.Router();
 var oracle = require('oracle');
 global.User = require('./user');
 
 //for dropdown things
-var friends = [];
 var dropdown = [];
-var uniqueFriends = [];
-var uniquePics = [];
 
 var connectData = {
 	hostname: "tripsterdb.cmjcmauyxdtp.us-east-1.rds.amazonaws.com",
@@ -19,6 +13,7 @@ var connectData = {
 	user: "masterusr",
 	password: "CS450&frdS"
 };
+
 
 function create_dropdown() {
 	oracle.connect(connectData, function (err, connection) {
@@ -41,7 +36,6 @@ function create_dropdown() {
 					console.log("Error executing query:", err);
 					return;
 				}
-				consoleprint
 				for (var i = 0; i < results.length; i++) {
 					var resul = results[i].FRIEND_ID1;
 					console.log("Friend: " + resul);
@@ -68,101 +62,118 @@ function create_dropdown() {
 }
 
 router.get('/createtrip', function(req, res) {
-	dropdown = [];
-	create_dropdown();
   	if (global.currUser.signed_in) {
 		//Render Createtrip page
-	  	res.render('createtrip', {
-	  		invitees: dropdown
-		});
+	  	res.render('createtrip', {});
 	} else {
     	res.redirect('/login');
 	}
 });
 
-//Find out if the location user has chosen already exists in database
-function location_exists(n) {
-
-	oracle.connect(connectData, function (err, connection) {
-		if (err) {
-			console.log("error connecting to db:", err);
-			return;
-		}
-		//if location does not exist in location table, add it
-		var loc_query = "SELECT * FROM LOCATION WHERE LOCATION_NAME ='" + n + "'";
-		connection.execute(loc_query, [], function(err, results) {
-			if (err) {
-				console.log('Error executing location query:', err);
-				return;
-			}
-
-		connection.close();
-		if (results.length == 0)
-			return false;
-		else 
-			return true;
-		});
-	});
-}
-
-//Create location in creation table
-function create_location(name, type) {
-	oracle.connect(connectData, function(err, connection){
-		if (err) {
-			console.log("Error connecting to db:", err);
-			return;
-		}
-
-		//Check if location type is null
-		if (type == '' || !type) {
-			type = "place";
-		}
-		var create_query = "INSERT INTO LOCATION (LOCATION_NAME, LOCATION_TYPE) VALUES";
-		create_query = create_query + "('" + name, +"', '"+ type +"')";
-		oracle.execute(create_query, [], function(err, results) {
-			if (err) {
-				console.log('Error executing location query:', err);
-				return;
-			}
-			console.log(results);
-			connection.close();
-		});
-	});
-}
-
 router.post('/createtrip', function (req, res) {
+	if (global.currUser.signed_in) {
+		var userid = currUser.username;
+		var planid = req.body.planid;
+		var privacycontent = req.body.privacycontent;
+		var locationname = req.body.locationname; //want to change location to country, if USA state + city, else city
+		var locationtype = req.body.locationtype;
+		var album = req.body.album;
+		var content = req.body.content;
+		var items = req.body.items;
+		var tripid;
 
-	var tripsize = 0;
-	var userid = global.currUser.username;
-	var tripid = tripsize;
-	var planid = 1;
-	var privacycontent = "'public'";
-	//want to change location to country, if USA state + city, else city
-	locationname = locationname.toLowerCase();
-	var locationtype = req.body.locationtype;
-	var invitees = req.body.invitees;
-	var album = req.body.album;
-	var content = req.body.content;
-	var items = req.body.items;
-	var queryresults;
+		//check if location exists, if not, create it in table
+		oracle.connect(connectData, function (err, connection) {
+			if (err) {
+				console.log("Error connecting to db creating trip:", err);
+				return;
+			}
+			//if location does not exist in location table, add it
+			var exists;
+			var loc_query = "SELECT * FROM LOCATION WHERE LOCATION_NAME='" + locationname + "'";
+				connection.execute(loc_query, [], function(err, results) {
+				if (err) {
+					console.log('Error executing location query:', err);
+					return;
+				}
 
-	console.log(locationtype);
-	oracle.connect(connectData, function (err, connection) {
-		if (err) {
-			console.log("Error connecting to db:", err);
-			return;
-		}
+				if (results.length === 0) {
+					if (!type) {
+						var create_query = "INSERT INTO LOCATION (LOCATION_NAME) VALUES";
+						create_query = create_query + "('" + locationname  +"')";
+						console.log(create_query);
+						connection.execute(create_query, [], function (err, results) {
+							console.log('here2');
+							if (err) {
+								console.log('Error executing location query:::', err);
+								return;
+							}
+							console.log(results);
+							connection.close();
+						});
+					} else {
+						var create_query = "INSERT INTO LOCATION (LOCATION_NAME, LOCATION_TYPE) VALUES";
+						create_query = create_query + " ('" + locationname +"', '" + locationtype +"')";
+						console.log(create_query);
+						connection.execute(create_query, [], function (err, results) {
+							console.log('here2');
+							if (err) {
+								console.log('Error executing location query:', err);
+								return;
+							}
+							console.log(results);
+						});
+					}
+				} 
+			});
+			//create trip id
+			var query = 'SELECT MAX(TRIP_ID) AS MAX FROM TRIPS';
+			connection.execute(query, [], function (err, results) {
+				if (err) {
+					console.log("Error executing query:", err);
+					return;
+				}
+				console.log("TRIP_ID: " + results[0].MAX);	//testing
+				console.log(results[0].MAX + 1);
+				tripid = results[0].MAX + 1;
 
-		//make sure location exists, if not, create it
-		var loc_exists = location_exists(locationame);
-		console.log(loc_exists);	//testing
-
-		if (!loc_exists) {
-			create_location(locationname, locationtype);
-		}
-
-		connection.close();
-	});
+				//create trip in database
+				var newtrip = "INSERT INTO TRIPS (TRIP_ID, LOCATION_ID, PRIVACY_CONTENT) VALUES('" + tripid 
+					+ "', '" + locationname + "', '" + privacycontent + "')";
+				console.log(newtrip);
+				connection.execute(newtrip, [], function (err, results) {
+					console.log('attempting query: ' + newtrip);
+					if (err) {
+						console.log("Error connecting to db:", err);
+						return;
+					}
+					console.log(results);
+				});
+				var planquery = "INSERT INTO PLANS values( '" + userid + "', " +
+					tripid + ", " + planid + ")";
+				connection.execute(planquery, [], function (err, results) {
+					console.log('attempting query: ' + planquery);
+					if (err) {
+						console.log("Error connecting to db:", err);
+						return;
+					}
+					console.log(planquery + "EXECUTED");
+					connection.close();
+					
+				});
+			});	
+		});
+		//render part II of trip requests - invite friends
+		dropdown = [];
+		create_dropdown();
+		res.render('invitefriends',{
+			invitees: dropdown,
+			tripid : tripid
+		});
+	} else {
+		res.redirect('/login');
+	}
 });
+
 
 module.exports = router;
